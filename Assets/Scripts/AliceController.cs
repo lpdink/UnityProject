950d3e9342982741d28a12c324034787d5c65efe
lpdink <326490366@qq.com>
2020-06-20 21:49:34 +0800

    lpdink说，生物应该学会自己移动。小狐狸可以通过wasd移动了！且移动与宇宙无关。（完成了教程的第二部分）

--- Assets/Scripts/AliceController.cs
+++ Assets/Scripts/AliceController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class AliceController : MonoBehaviour
@@ -8,14 +10,21 @@ public class AliceController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0; 
+        //Application.targetFrameRate = 60;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        //UnityEngine.Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 10f * horizontal*Time.deltaTime;
+        position.y = position.y + 10f * vertical*Time.deltaTime;
+        UnityEngine.Debug.Log(Time.deltaTime);
         transform.position = position;
     }
 }
